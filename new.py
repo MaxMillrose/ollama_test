fa6e6cd67801abb6249b8815ff31b9a946a09136
Max.Millrose <max.millrose@mailbox.org>
2024-10-27 13:48:45 +0100

    Minor changes. WIP

--- new.py
+++ new.py
@@ -12,7 +12,9 @@ from langchain.chains import LLMChain
 
 
 
+
 ollama_embbed_model = "mxbai-embed-large:latest"
+
 ollama_query_model = "wizard-vicuna-uncensored:30b"
 pdf_path="./"
 chroma_path="./vectorstore"
@@ -21,7 +23,7 @@ text_splitter = SemanticChunker(
     embeddings=OEmbed,
     breakpoint_threshold_type="gradient", 
     breakpoint_threshold_amount=50.0)
-llm = OllamaLLM(model=ollama_query_model, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), temperature=0.9)
+llm = OllamaLLM(model=ollama_embbed_model, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]), temperature=0.9)
  
 def load_vecstore(chroma_path):
     if os.path.exists(chroma_path):
@@ -36,7 +38,8 @@ def load_pdfs(pdf_path):
     docs = loader.load()  # Load all PDF files
     
     if not docs:
-        print("No documents loaded from the specified PDFs.")
+        print(f"No documents loaded from the specified PDFs.")
+        print(f"please check {pdf_path} for PDFs.")
         sys.exit(1)  # Exit if no documents are loaded
     else:
         print(f"Number of PDF files loaded: {len(docs)}")
@@ -54,6 +57,8 @@ def splitting(docs):
 
 def questions(vectorstore):
     print(f"Asking our questions")
+    print(f"NEXT TIME. EXIT for now")
+    sys.exit(1)
     
     prompt = PromptTemplate(
         input_variables=["topic"],
@@ -78,12 +83,12 @@ def main():
     
     if not vectorstore:
         print(f"Found no existing vectorstore. ")
+        print(f"Reparsing documents ")
         vectorstore = splitting(load_pdfs(pdf_path))
-        print(vectorstore._collection.get())
-    else:
-        print("Check collection and PDFs")    
-        print(vectorstore._collection.get(metadatas))
-
+        print(vectorstore._collection.get(include=))
+    else: 
+        print(vectorstore.get)
+    
     questions(vectorstore)
     
 
